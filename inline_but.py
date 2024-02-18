eaea0abf35d904cd36d1ea24fe852a4885651e11
kali <3242@djffg.cri>
2024-02-18 17:41:39 +1000

    language

--- inline_but.py
+++ inline_but.py
@@ -1,11 +1,64 @@
 from aiogram.utils.keyboard import InlineKeyboardBuilder
 from config import garant
+from translate import _
+                ### ТО, ЧТО ВИДЕТ КЛИЕНТ ###
+def start_but(lang):
+    st = InlineKeyboardBuilder()
+    st.button(text=f"{_('Обмен', lang)}", callback_data="exch")
+    st.button(text=f"{_('Гарант сделок', lang)}", url=f"{garant[0][1]}")
+    st.button(text=f"{_('Наш чат', lang)}", url="google.com")
+    st.button(text=f"{_('Наш канал', lang)}", url="google.com")
+    st.button(text=f"{_('Связь с админом', lang)}", url="google.com")
+    st.adjust(1, 1, 2)
+    return st
+
+
+def rules(lang):
+    rul = InlineKeyboardBuilder()
+    rul.button(text=f"{_('Согласен', lang)}🟢", callback_data="agree_rules")
+    rul.adjust(1)
+    return rul
+
+
+def lang_btn():
+    lang = InlineKeyboardBuilder()
+    lang.button(text="Русский", callback_data="lang_RU")
+    lang.button(text="English", callback_data="lang_EN")
+    lang.adjust(2)
+    return lang
+
+                ### АДМИН ПАНЕЛЬ ###
+def admin_but_send():
+    adm = InlineKeyboardBuilder()
+    adm.button(text="Рассылка с фотографией", callback_data="send1")
+    adm.button(text="Рассылка без фотографии", callback_data="send2")
+    adm.adjust(1)
+    return adm
+
+
 def admin_but():
     adm = InlineKeyboardBuilder()
     adm.button(text="Админка обменника", callback_data="adm_exc")
     adm.button(text="Рассылка", callback_data="send")
     adm.adjust(1)
     return adm
+
+
+def admin_bc_fsm():
+    adm = InlineKeyboardBuilder()
+    adm.button(text="Да✅", callback_data="yes")
+    adm.button(text="Нет❌", callback_data="no")
+    adm.adjust(1)
+    return adm
+
+
+def admin_bc_fsm2():
+    adm = InlineKeyboardBuilder()
+    adm.button(text="Да✅", callback_data="yes2")
+    adm.button(text="Нет❌", callback_data="no2")
+    adm.adjust(1)
+    return adm
+
 
 def admin_exc():
     adm_exc = InlineKeyboardBuilder()
@@ -14,38 +67,3 @@ def admin_exc():
     adm_exc.button(text="Назад🔙", callback_data="back_admin")
     adm_exc.adjust(1, 1, 1)
     return adm_exc
-def rules():
-    rul = InlineKeyboardBuilder()
-    rul.button(text="Согласен🟢", callback_data="agree_rules")
-    rul.adjust(1)
-    return rul
-
-
-def start_but():
-    st = InlineKeyboardBuilder()
-    st.button(text="Обмен", callback_data="exch")
-    st.button(text="Сделка с Гарантом", url=f"{garant[0][1]}")
-    st.button(text="Наш чат", url="google.com")
-    st.button(text="Наш канал", url="google.com")
-    st.button(text="Связь с админом", url="google.com")
-    st.adjust(1, 1, 2)
-    return st
-def admin_but_send():
-    adm = InlineKeyboardBuilder()
-    adm.button(text="Рассылка с фотографией", callback_data="send1")
-    adm.button(text="Рассылка без фотографии", callback_data="send2")
-    adm.adjust(1)
-    return adm
-
-def admin_bc_fsm():
-    adm = InlineKeyboardBuilder()
-    adm.button(text="Да✅", callback_data="yes")
-    adm.button(text="Нет❌", callback_data="no")
-    adm.adjust(1)
-    return adm
-def admin_bc_fsm2():
-    adm = InlineKeyboardBuilder()
-    adm.button(text="Да✅", callback_data="yes2")
-    adm.button(text="Нет❌", callback_data="no2")
-    adm.adjust(1)
-    return adm
--- routers.py
+++ routers.py
@@ -13,12 +13,25 @@ async def start_db(user_id, f_name, l_name):
 
 async def check_us(us_id):
     try:
-        return curs.execute("SELECT * FROM users WHERE id_us = ?", (us_id,)).fetchone()
+        return curs.execute("SELECT id_us FROM users WHERE id_us = ?", (us_id,)).fetchone()
     except:
         pass  # logging в файлы
 
 async def check_tech():
     try:
         return curs.execute("SELECT * FROM technical").fetchone()
+    except:
+        pass
+
+async def add_lang(lang, id_us):
+    try:
+        curs.execute("UPDATE users SET lang = ? WHERE id_us = ?", (lang, id_us))
+        conn.commit()
+    except:
+        pass
+
+async def check_lang(id_us):
+    try:
+        return curs.execute("SELECT lang FROM users WHERE id_us = ?", (id_us, )).fetchone()
     except:
         pass
